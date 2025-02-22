3d27272ad48ea392f7b9612136953fbe3737827e
Saepul Bahri <bahreesaepul1@gmail.com>
2025-02-22 19:44:44 +0700

    added local fonts and load it to the app

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,25 @@
-import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import { useFonts } from 'expo-font';
+import { SplashScreen, Stack } from 'expo-router';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+	const [loaded, error] = useFonts({
+		'Nunito-Regular': require('../assets/fonts/Nunito-Regular.ttf'),
+		'Nunito-SemiBold': require('../assets/fonts/Nunito-SemiBold.ttf'),
+	});
+
+	useEffect(() => {
+		if (loaded || error) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded, error]);
+
+	// return nothing if assets are not loaded
+	if (!loaded && !error) {
+		return null;
+	}
+
 	return <Stack />;
 }
